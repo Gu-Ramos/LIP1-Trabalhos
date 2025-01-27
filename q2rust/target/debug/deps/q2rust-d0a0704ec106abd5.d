76c2d3f092ffa207262321e3de94bff3972c09b5
Ramos <gu.ramos@proton.me>
2025-01-27 00:20:37 -0300

    ops

--- q2rust/target/debug/deps/q2rust-d0a0704ec106abd5.d
+++
@@ -1,6 +0,0 @@
-/home/ramos/Desktop/LIP1/LIP1-Trabalhos/q2rust/target/debug/deps/libq2rust-d0a0704ec106abd5.rmeta: src/main.rs src/parser.rs
-
-/home/ramos/Desktop/LIP1/LIP1-Trabalhos/q2rust/target/debug/deps/q2rust-d0a0704ec106abd5.d: src/main.rs src/parser.rs
-
-src/main.rs:
-src/parser.rs:
